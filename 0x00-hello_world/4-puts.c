e9b54d96d514c072ca95f90b85ecbb3863c185b1
SirySand <georgessand11@gmail.com>
2022-10-17 20:06:46 -0700

    space before (

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
-
+/*
+ * main - print a phrase using puts
+ * don't use printf
+ * return (zero)
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
