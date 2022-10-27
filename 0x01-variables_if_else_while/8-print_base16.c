452776ff4538e676639bd2e4b8888e9c18e5b0b9
SirySand <georgessand11@gmail.com>
2022-10-27 08:04:13 -0700

    numbers in base 16 lowercase printing

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	char d;
+
+	int i;
+
+	d = 'a';
+	while
+		(i < 10) {
+		       putchar(i + '0');
+			i++;
+		}
+	while
+		(d <= 'f') {
+			putchar(d);
+			d++;
+		}
+	putchar('\n');
+	return (0);
+}
+
+
