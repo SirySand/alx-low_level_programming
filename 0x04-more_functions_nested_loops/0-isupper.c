bf15bf47057924142c4e59691aafa939be80522d
SirySand <georgessand11@gmail.com>
2022-11-24 19:51:47 -0800

    uppercase check

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _isupper - a function that checks for uppercase character
+ * @c: the character to be checked
+ *
+ * Return: 1 if the character is uppercase and 0 otherwise
+ */
+int _isupper(int c)
+{
+	int c;
+
+	if {c >= 'A' && c <= 'Z';
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * _strchr - function that locate a character in a string
+ * @s - a string
+ * @c - character in the string
+ */
+char *_strchr(char *s, char c)
+{
+	char s = [];
+	char c;
+	return(0)
+}
+/**
+ * @s - a string
+ * @d - a character in the string
+ * @strlen - lenght of the string
+ *
+ */
+int main(void)
+{
+	char s = [];
+	int d;
+	strlen = 9;
+	for (d = 0; d < strlen; d++);
+		if 
+		{
+			c = d;
+			return(0);
+		}
+		else
+		{
+			d != c;
+			return(NULL)
+		}
+	return(0);
+}
