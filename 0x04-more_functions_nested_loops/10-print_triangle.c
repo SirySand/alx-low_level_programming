3d8377d3d094d9cf6f8224f95b4540ca8d78acfe
SirySand <georgessand11@gmail.com>
2022-12-15 14:12:48 -0800

    puts_rec

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_triangle - function that print triangle
+ */
+void print_triangle(int size)
+{
+	int i;
+
+	for
--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,14 +1,21 @@
 #include "main.h"
 #include <stdio.h>
 
-/*
+/**
  * _puts_recursion - writes a string and a trailing newline to stdout.
- * main - check the code
- * @*s - string
- * Return: Always 0.
+ * @s: string to be printed
+ * Return: Always 0 (success)
  */
-int main(void)
+void _puts_recursion(char *s)
 {
-	_puts_recursion("");
-	return (0);
+	*s = getchar();
+
+	if (*s != '\n')
+	{
+		putchar('s');
+	}
+	else
+	{
+		putchar('\n');
+	}
 }
